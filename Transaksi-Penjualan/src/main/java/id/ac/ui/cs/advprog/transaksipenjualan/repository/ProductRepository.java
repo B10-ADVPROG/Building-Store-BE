4c92e8702546433ccc0ad46f459b74b4757ff7e1
nadyaaysha <nadyaaysha07@gmail.com>
2025-04-08 15:36:07 +0700

    Made Product Repository

---
+++ Transaksi-Penjualan/src/main/java/id/ac/ui/cs/advprog/transaksipenjualan/repository/ProductRepository.java
@@ -0,0 +1,32 @@
+package id.ac.ui.cs.advprog.transaksipenjualan.repository;
+
+import id.ac.ui.cs.advprog.transaksipenjualan.model.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Repository
+public class ProductRepository {
+    private List<Product> productData = new ArrayList<>();
+
+    public Product create(Product product) {
+        product.setProductId(java.util.UUID.randomUUID().toString());
+        productData.add(product);
+        return product;
+    }
+
+    public Iterator<Product> findAll() {
+        return productData.iterator();
+    }
+
+    public Product findById(String productId) {
+        for (Product product : productData) {
+            if (product.getProductId().equals(productId)) {
+                return product;
+            }
+        }
+        return null;
+    }
+}
