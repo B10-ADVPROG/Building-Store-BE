a52b582009596b5123fe14e9a6f845e5aeabc7e7
nadyaaysha <nadyaaysha07@gmail.com>
2025-04-08 12:26:47 +0700

    Initialization Transaksi Penjualan

---
+++ Transaksi-Penjualan/src/main/java/id/ac/ui/cs/advprog/transaksipenjualan/TransaksiPenjualanApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.transaksipenjualan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransaksiPenjualanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransaksiPenjualanApplication.class, args);
+    }
+
+}
