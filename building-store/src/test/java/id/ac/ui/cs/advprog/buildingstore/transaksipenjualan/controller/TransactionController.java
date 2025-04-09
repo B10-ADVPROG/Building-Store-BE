874bc33a872ca5ec1fd0fc09d9aed4ff1f63e425
William Alexander <williamax2005@gmail.com>
2025-04-09 09:10:21 +0700

    Create feature packages template on test

---
+++ building-store/src/test/java/id/ac/ui/cs/advprog/buildingstore/transaksipenjualan/controller/TransactionController.java
@@ -0,0 +1,37 @@
+package id.ac.ui.cs.advprog.buildingstore.transaksipenjualan.controller;
+
+import id.ac.ui.cs.advprog.buildingstore.transaksipenjualan.model.Transaction;
+import id.ac.ui.cs.advprog.buildingstore.transaksipenjualan.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/transaction")
+public class TransactionController {
+    @Autowired
+    private TransactionService service;
+
+    @GetMapping("/create")
+    public String createTransactionPage(Model model) {
+        Transaction transaction = new Transaction();
+        model.addAttribute("transaction", transaction);
+        return "CreateTransaction";
+    }
+
+    @PostMapping("/create")
+    public String createTransactionPost(@ModelAttribute Transaction transaction, Model model) {
+        service.create(transaction);
+        return "redirect:list";
+    }
+
+    @GetMapping("/list")
+    public String transactionListPage(Model model) {
+        List<Transaction> allTransaction = service.findAll();
+        model.addAttribute("transactions", allTransaction);
+        return "TransactionList";
+    }
+}
