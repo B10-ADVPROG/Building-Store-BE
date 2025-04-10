7549206b15561193be1340775591c4ebce0e17ee
William Alexander <williamax2005@gmail.com>
2025-04-10 19:46:18 +0700

    [GREEN] Implement product service

---
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/manajemenproduk/service/ProductServiceImpl.java
@@ -0,0 +1,46 @@
+package id.ac.ui.cs.advprog.buildingstore.manajemenproduk.service;
+
+import id.ac.ui.cs.advprog.buildingstore.manajemenproduk.model.Product;
+import id.ac.ui.cs.advprog.buildingstore.manajemenproduk.repository.ProductRepository;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ProductServiceImpl implements ProductService {
+    private ProductRepository repository;
+
+    public void setRepository(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Product create(Product product) {
+        return repository.create(product);
+    }
+
+    @Override
+    public List<Product> findAll() {
+        Iterator<Product> iterator = repository.findAll();
+        List<Product> result = new ArrayList<>();
+        iterator.forEachRemaining(result::add);
+        return result;
+    }
+
+    @Override
+    public Product findById(String id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public Product update(String id, Product updatedProduct) {
+        return repository.update(id, updatedProduct);
+    }
+
+    @Override
+    public void delete(String id) {
+        repository.delete(id);
+    }
+
+
+}
