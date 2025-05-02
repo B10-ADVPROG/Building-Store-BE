08f187604546b99f8064fb3ee0dcc470e026256d
William Alexander <williamax2005@gmail.com>
2025-05-02 20:46:52 +0700

    Fix duplicate controller issues

--- src/main/java/id/ac/ui/cs/advprog/buildingstore/manajemenproduk/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/manajemenproduk/service/ProductServiceImpl.java
@@ -2,11 +2,13 @@ package id.ac.ui.cs.advprog.buildingstore.manajemenproduk.service;
 
 import id.ac.ui.cs.advprog.buildingstore.manajemenproduk.model.Product;
 import id.ac.ui.cs.advprog.buildingstore.manajemenproduk.repository.ProductRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+@Service
 public class ProductServiceImpl implements ProductService {
     private ProductRepository repository;
 
