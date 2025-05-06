36eff209cdbe155ce19b113a282cd556d5db5789
William Alexander <williamax2005@gmail.com>
2025-05-06 23:24:35 +0700

    Create test for authentication models

--- src/test/java/id/ac/ui/cs/advprog/buildingstore/authentication/model/KasirTest.java
+++ src/test/java/id/ac/ui/cs/advprog/buildingstore/authentication/model/KasirTest.java
@@ -1,4 +1,21 @@
 package id.ac.ui.cs.advprog.buildingstore.authentication.model;
 
-public class KasirTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class KasirTest {
+
+    @Test
+    void testKasirConstructor() {
+        String email = "kasir@example.com";
+        String fullname = "Kasir User";
+        String password = "kasirpassword";
+
+        Kasir kasir = new Kasir(email, fullname, password);
+
+        assertEquals(email, kasir.getEmail());
+        assertEquals(fullname, kasir.getFullname());
+        assertEquals(password, kasir.getPassword());
+        assertEquals("Kasir", kasir.getRole());
+    }
 }
