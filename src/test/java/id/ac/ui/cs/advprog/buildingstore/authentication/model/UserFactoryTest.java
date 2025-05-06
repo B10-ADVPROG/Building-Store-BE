82ebac7be33b6cfddf67afb8d94fcb5816b81ccb
William Alexander <williamax2005@gmail.com>
2025-05-06 22:44:13 +0700

    [RED] Create test for user factory pattern

---
+++ src/test/java/id/ac/ui/cs/advprog/buildingstore/authentication/model/UserFactoryTest.java
@@ -0,0 +1,35 @@
+package id.ac.ui.cs.advprog.buildingstore.authentication.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserFactoryTest {
+
+    @Test
+    public void testCreateKasir() {
+        User kasir = UserFactory.createUser("kasir", "kasir@gmail.com", "Kasir Test", "kasirpass");
+
+        assertInstanceOf(Kasir.class, kasir);
+        assertEquals("Kasir", kasir.getRole().getDisplayName());
+        assertEquals("kasir@gmail.com", kasir.getEmail());
+    }
+
+    @Test
+    public void testCreateAdministrator() {
+        User admin = UserFactory.createUser("administrator", "admin@gmail.com", "Admin Test", "adminpass");
+
+        assertInstanceOf(Administrator.class, admin);
+        assertEquals("Administrator", admin.getRole().getDisplayName());
+        assertEquals("admin@gmail.com", admin.getEmail());
+    }
+
+    @Test
+    public void testInvalidRoleThrowsException() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            UserFactory.createUser("manager", "manager@gmail.com", "Manager Test", "managerpass");
+        });
+
+        assertEquals("Unknown role: manager", exception.getMessage());
+    }
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/buildingstore/authentication/model/UserTest.java
@@ -0,0 +1,4 @@
+package id.ac.ui.cs.advprog.buildingstore.authentication.model;
+
+public class UserTest {
+}
