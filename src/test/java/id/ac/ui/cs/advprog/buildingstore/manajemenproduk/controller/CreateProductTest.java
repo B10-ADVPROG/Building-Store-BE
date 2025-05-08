1b46215452845d63afd11b9a36b3834d62e0ae7f
William Alexander <williamax2005@gmail.com>
2025-05-08 20:30:53 +0700

    [RED] Create test for view products

--- src/test/java/id/ac/ui/cs/advprog/buildingstore/manajemenproduk/controller/CreateProductTest.java
+++ src/test/java/id/ac/ui/cs/advprog/buildingstore/manajemenproduk/controller/CreateProductTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -29,7 +30,7 @@ public class CreateProductTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private ProductService productService;
 
     private String validToken;
