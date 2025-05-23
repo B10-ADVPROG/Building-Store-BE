80adef86059a10402777dbd8e9638dcc9b3d5ea3
William Alexander <williamax2005@gmail.com>
2025-05-24 03:09:19 +0700

    [GREEN] Implement transaction repository

---
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/transaksipenjualan/repository/TransactionRepository.java
@@ -0,0 +1,38 @@
+package id.ac.ui.cs.advprog.buildingstore.transaksipenjualan.repository;
+
+import id.ac.ui.cs.advprog.buildingstore.transaksipenjualan.model.SalesTransaction;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class TransactionRepository {
+
+    private final Map<String, SalesTransaction> storage = new HashMap<>();
+
+    public SalesTransaction save(SalesTransaction transaction) {
+        storage.put(transaction.getTransactionId(), transaction);
+        return transaction;
+    }
+
+    public SalesTransaction findById(String id) {
+        return storage.get(id);
+    }
+
+    public List<SalesTransaction> findAll() {
+        return new ArrayList<>(storage.values());
+    }
+
+    public void delete(String id) {
+        storage.remove(id);
+    }
+
+    public void update(String id, SalesTransaction updatedTransaction) {
+        if (storage.containsKey(id)) {
+            storage.put(id, updatedTransaction);
+        }
+    }
+}
